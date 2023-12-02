1e3b72e9fc3314f0bc66d02630620847e02c1f91
LaM0uette <LaM0uette.dev@gmail.com>
2023-12-02 17:36:12 +0100

    up

--- Trebuchet/Program.cs
+++ Trebuchet/Program.cs
@@ -2,9 +2,19 @@
 
  public static class Program
  {
+     #region Statements
+
      public static void Main(string[] args)
      {
          // ...
      }
+
+     #endregion
+
+     #region Functions
+
+     
+
+     #endregion
  }
  
