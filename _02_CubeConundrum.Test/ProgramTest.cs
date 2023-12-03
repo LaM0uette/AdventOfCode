24bf5dd62b7bc9479331b8019e9b68e5c514ac40
LaM0uette <LaM0uette.dev@gmail.com>
2023-12-03 10:05:12 +0100

    add _02_CubeConundrum project

---
+++ _02_CubeConundrum.Test/ProgramTest.cs
@@ -0,0 +1,9 @@
+namespace _02_CubeConundrum.Test;
+
+public class ProgramTest
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ _02_CubeConundrum/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _02_CubeConundrum;
+
+public static class Program
+{
+    public static void Main()
+    {
+        const string file = "data.txt";
+        var lines = File.ReadAllLines(file);
+        
+        
+    }
+}
