5efb38719be35e47befe8058a07eb55b895863bf
LaM0uette <LaM0uette.dev@gmail.com>
2023-12-03 16:14:00 +0100

    part 1 ok

--- _03_GearRatios/Program.cs
+++ _03_GearRatios/Program.cs
@@ -4,9 +4,10 @@ public static class Program
 {
     public static void Main()
     {
-        const string file = "test.txt";
+        const string file = "data.txt";
         var lines = File.ReadAllLines(file);
         
         var result = SchematicParser.CalculatePartNumbersSum(lines);
+        Console.WriteLine(result);
     }
 }
--- _03_GearRatios/SchematicParser.cs
+++ _03_GearRatios/SchematicParser.cs
@@ -2,30 +2,85 @@
 
 namespace _03_GearRatios;
 
+public struct Part
+{
+    public int Number { get; set; }
+    public int Index { get; set; }
+    public int Length { get; set; }
+}
+
 public static class SchematicParser
 {
     public static int CalculatePartNumbersSum(string[] schematic)
     {
+        var result = 0;
         for (var i = 0; i < schematic.Length; i++)
         {
-            ExtractNumbersAndIndices(schematic[i]);
-            
-            for (var j = 0; j < schematic.Length; j++)
+            var parts = ExtractNumbersAndIndices(schematic[i]);
+
+            foreach (var part in parts)
             {
-                
+                if (LookAround(schematic, i, part))
+                {
+                    result += part.Number;
+                }
             }
         }
 
-        return 0;
+        return result;
     }
 
-    private static void ExtractNumbersAndIndices(string input)
+    private static List<Part> ExtractNumbersAndIndices(string input)
     {
         var regex = new Regex(@"\d+");
 
+        var parts = new List<Part>();
         foreach (Match match in regex.Matches(input))
         {
-            Console.WriteLine($"{match.Value} index {match.Index}");
+            parts.Add(new Part
+            {
+                Number = int.Parse(match.Value),
+                Index = match.Index,
+                Length = match.Length,
+            });
         }
+
+        return parts;
+    }
+
+    private static bool LookAround(IReadOnlyList<string> schematic, int row, Part part)
+    {
+        for (var i = 0; i < part.Length; i++)
+        {
+            if (IsAdjacentToSymbol(schematic, row, part.Index + i))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static bool IsAdjacentToSymbol(IReadOnlyList<string> schematic, int row, int col)
+    {
+        for (var i = -1; i <= 1; i++)
+        {
+            for (var j = -1; j <= 1; j++)
+            {
+                if (i == 0 && j == 0) continue;
+
+                var newRow = row + i;
+                var newCol = col + j;
+
+                if (newRow >= 0 && newRow < schematic.Count && newCol >= 0 && newCol < schematic[newRow].Length)
+                {
+                    var c = schematic[newRow][newCol];
+                    if (c != '.' && !char.IsDigit(c))
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 }
